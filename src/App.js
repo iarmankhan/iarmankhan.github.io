1e2d8950123540b377378dfccea80cde08a4b2f2
iarmankhan <armankhan9244@gmail.com>
2020-03-12 19:38:07 +0530

    App setup, folder structure setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import './assets/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+        <div className='app'>
+          <Switch>
+            {/*<Route exact path='/' />*/}
+          </Switch>
+        </div>
+    );
+  }
 }
 
 export default App;
