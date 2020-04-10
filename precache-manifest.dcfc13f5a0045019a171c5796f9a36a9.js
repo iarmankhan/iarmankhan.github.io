3c038effd0e5d874b2439ea63b810be9aab00995
iarmankhan <armankhan9244@gmail.com>
2020-04-10 22:03:46 +0530

    Updates

---
+++ precache-manifest.dcfc13f5a0045019a171c5796f9a36a9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161175146efbefed8388f420b29251a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "122687af5b5d763fbbd4",
+    "url": "/static/css/main.8d7eb8cf.chunk.css"
+  },
+  {
+    "revision": "002118344788711bf518",
+    "url": "/static/js/2.cc3947d5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.cc3947d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122687af5b5d763fbbd4",
+    "url": "/static/js/main.c5202d2e.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "cdb776cd73f0e358b9e480ab68617532",
+    "url": "/static/media/bio.cdb776cd.jpg"
+  },
+  {
+    "revision": "24eed9e9c01c6fae3262c243da3ba565",
+    "url": "/static/media/clover-clothing.24eed9e9.png"
+  },
+  {
+    "revision": "4ab8e04efca4897a59adc5f90ce36404",
+    "url": "/static/media/home.4ab8e04e.jpg"
+  },
+  {
+    "revision": "d9a26cee1ede83e3660eecbe4143f0d0",
+    "url": "/static/media/logo.d9a26cee.png"
+  },
+  {
+    "revision": "0a6dd2e6178df1ede095985591c2923e",
+    "url": "/static/media/picsagram.0a6dd2e6.png"
+  },
+  {
+    "revision": "67f58e9a1feee5fec89f3a218284a036",
+    "url": "/static/media/portfolio-project.67f58e9a.png"
+  },
+  {
+    "revision": "d7c2385b41ac773f201b3918eebdab69",
+    "url": "/static/media/qa-forum.d7c2385b.png"
+  },
+  {
+    "revision": "0bfd08b7d67a582c38799c4073a21b02",
+    "url": "/static/media/shop-app.0bfd08b7.png"
+  },
+  {
+    "revision": "701843ff37e1914212e42486cc8fb51d",
+    "url": "/static/media/wp-social-connect.701843ff.png"
+  }
+]);
