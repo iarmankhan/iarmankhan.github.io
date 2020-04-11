1798c4eda8e00122ecfbb3db77a0d67b8cda5cd0
iarmankhan <armankhan9244@gmail.com>
2020-04-11 12:48:10 +0530

    Changed browser router with hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 import './css/index.css';
 import App from './App';
@@ -8,8 +8,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <App />
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 
 serviceWorker.unregister();
